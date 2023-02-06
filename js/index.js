9688d642ba0a7df996a90b82cbed83c7389218fe
Davi Placco <davibplacco@gmail.com>
2023-02-06 20:48:49 -0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,104 @@
+// API request URL
+
+// API from "awesomeapi"
+
+const url = 'https://economia.awesomeapi.com.br/last/'
+const coins = 'USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL'
+
+fetch(url + coins)
+  .then(function(responsive){
+    return responsive.json()
+  })
+  .then(function(data){
+    dolarReal = data.USDBRL;
+    euroReal = data.EURBRL;
+    libraReal = data.GBPBRL;
+
+    console.log(dolarReal , euroReal , libraReal);
+
+    var resultado = document.querySelector('#resultado');
+    var real = document.querySelector('#insira-o-valor');
+
+    var converterparadolar = document.querySelector('#converter-para-dolar');
+    var converterparaeuro = document.querySelector('#converter-para-euro');
+    var converterparalibra = document.querySelector('#converter-para-libra');
+
+    var resumocontainer = document.querySelector('#resumo');
+    var resumo = document.querySelector('#titulo-resumo');
+    var alta = document.querySelector('#alta-valor');
+    var baixa = document.querySelector('#baixa-valor');
+    var variacao = document.querySelector('#variacao-valor');
+    var compra = document.querySelector('#compra-valor');
+
+    var fechar = document.querySelector('#close');
+
+
+
+    // Dolar converter
+
+    converterparadolar.addEventListener('click', function(){
+       
+        resultadodolar = real.value / dolarReal.high;
+        resultado.textContent = resultadodolar.toLocaleString('pt-BR', {style: 'currency', currency: 'USD'});
+        
+        resumocontainer.style.opacity = "1";
+        resumocontainer.style.transition = "600ms ease-in-out";
+
+        resumo.innerHTML = 'Resumo Dolar';
+        alta.innerHTML = 'R$ ' + dolarReal.high;
+        baixa.innerHTML = 'R$ ' + dolarReal.low;
+        variacao.innerHTML = 'R$ ' + dolarReal.varBid;
+        compra.innerHTML = 'R$ ' + dolarReal.bid;
+
+
+        console.log(resultadodolar.toLocaleString('pt-BR', {style: 'currency', currency: 'USD'}));
+    })
+
+    // Euro converter
+
+    converterparaeuro.addEventListener('click', function(){
+       
+        resultadoeuro = real.value / euroReal.high;
+        resultado.textContent = resultadoeuro.toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'});
+
+        resumocontainer.style.opacity = "1";
+        resumocontainer.style.transition = "600ms ease-in-out";
+
+        resumo.innerHTML = 'Resumo Euro';
+        alta.innerHTML = 'R$ ' + euroReal.high;
+        baixa.innerHTML = 'R$ ' + euroReal.low;
+        variacao.innerHTML = 'R$ ' + euroReal.varBid;
+        compra.innerHTML = 'R$ ' + euroReal.bid;
+
+
+        console.log(resultadoeuro.toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'}));
+    })
+
+    // Libra converter
+
+    converterparalibra.addEventListener('click', function(){
+       
+        resultadolibra = real.value / libraReal.high;
+        resultado.textContent = resultadolibra.toLocaleString('pt-BR', {style: 'currency', currency: 'GBP'});
+
+        resumocontainer.style.opacity = "1";
+        resumocontainer.style.transition = "600ms ease-in-out";
+        
+        resumo.innerHTML = 'Resumo Libra';
+        alta.innerHTML = 'R$ ' + libraReal.high;
+        baixa.innerHTML = 'R$ ' + libraReal.low;
+        variacao.innerHTML = 'R$ ' + libraReal.varBid;
+        compra.innerHTML = 'R$ ' + libraReal.bid;
+
+
+        console.log(resultadolibra.toLocaleString('pt-BR', {style: 'currency', currency: 'GBP'}));
+    })
+
+    // Close resumo
+
+    fechar.addEventListener('click', function(){
+        resumocontainer.style.opacity = "0";
+    })
+
+  }
+  )
---
+++ readme.md
@@ -0,0 +1,127 @@
+# Currency Converter in JavaScript
+
+### This is a simple Currency Converter built using JavaScript. It allows the user to convert an amount from one currency to another.
+
+## Desktop
+
+![](/IMAGES/screenshot.png)
+
+## IOS
+
+![](/IMAGES/ios.png)
+
+## Android mobile
+
+![](/IMAGES/mobile.png)
+
+
+
+
+## Features
+
+* Real-time exchange rate conversion
+* Supports multiple currencies (API awesome)
+* User-friendly interface
+* Responsive interface
+* Painel with rate High and Low
+
+## Usage
+
+1. Check the link: https://.....
+2. Or clone the files and save in your device
+3. For the interaction, it`s simple select the currency you want to convert from and to
+
+## Technologies 
+
+* JavaScript
+* HTML5
+* CSS3 - LESS
+* API - apiawesome
+* Figma
+* Wepkip
+
+## API information
+
+* link to API: (https://docs.awesomeapi.com.br/api-de-moedas)
+
+* Returns the last occurrence of the selected currencies.
+Ex.: (https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL)
+
+* Parameters - 200 responses 
+
+~~~~ javascript
+
+{
+    "USDBRL": {
+        "code": "USD",
+        "codein": "BRL",
+        "name": "Dólar Americano/Real Brasileiro",
+        "high": "5.734",
+        "low": "5.7279",
+        "varBid": "-0.0054",
+        "pctChange": "-0.09",
+        "bid": "5.7276",
+        "ask": "5.7282",
+        "timestamp": "1618315045",
+        "create_date": "2021-04-13 08:57:27"
+    },
+    "EURBRL": {
+        "code": "EUR",
+        "codein": "BRL",
+        "name": "Euro/Real Brasileiro",
+        "high": "6.8327",
+        "low": "6.8129",
+        "varBid": "-0.0069",
+        "pctChange": "-0.1",
+        "bid": "6.8195",
+        "ask": "6.822",
+        "timestamp": "1618315093",
+        "create_date": "2021-04-13 08:58:15"
+    },
+    "BTCBRL": {
+        "code": "BTC",
+        "codein": "BRL",
+        "name": "Bitcoin/Real Brasileiro",
+        "high": "360000",
+        "low": "340500",
+        "varBid": "17072.9",
+        "pctChange": "4.98",
+        "bid": "359973.9",
+        "ask": "359974",
+        "timestamp": "1618315092",
+        "create_date": "2021-04-13 08:58:12"
+    }
+}
+
+~~~~
+
+* Parameters - 404 responses
+
+~~~~ javascript
+
+{
+    "status": 404,
+    "code": "CoinNotExists",
+    "message": "moeda nao encontrada ABC-BRL"
+}
+
+~~~~
+
+* Objects console.lo() 
+
+![](/IMAGES/console.log.png)
+
+* See the full list of combinations at: (https://economia.awesomeapi.com.br/xml/available)
+* See list of currency names: (https://economia.awesomeapi.com.br/xml/available/uniq)
+
+## Future Plans
+
+* Add more currencies to the list of supported currencies
+* Improve the UI for better user experience
+* Add a feature to save the conversion history for future reference
+
+## Contribution
+
+### Feel free to contribute to this project by submitting pull requests. Any suggestions or improvements are welcome.
+
+## Developed by Davi B. Placco!
